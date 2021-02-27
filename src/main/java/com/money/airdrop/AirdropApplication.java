a7f73a60c03aebf074a5bdc2e76c2fc0e73b1305
wrpark <wrpark@blocko.io>
2021-02-27 17:59:43 +0900

    Add initializr sources

---
+++ src/main/java/com/money/airdrop/AirdropApplication.java
@@ -0,0 +1,13 @@
+package com.money.airdrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirdropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirdropApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/money/airdrop/AirdropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.money.airdrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirdropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
