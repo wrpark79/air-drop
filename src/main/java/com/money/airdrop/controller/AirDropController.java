579299f2ded3fbfb1e069531d9cbd662266bfd6a
wrpark <wrpark@blocko.io>
2021-02-28 12:02:09 +0900

    Add swagger

--- src/main/java/com/money/airdrop/controller/AirDropController.java
+++ src/main/java/com/money/airdrop/controller/AirDropController.java
@@ -1,6 +1,7 @@
 package com.money.airdrop.controller;
 
 import com.money.airdrop.service.AirDropService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ public class AirDropController {
         this.airDropService = airDropService;
     }
 
+    @ApiOperation(value = "Create an airdrop event", notes = "뿌리기 이벤트 생성")
     @PostMapping("/airdrops")
     public String send(
         @RequestHeader(name = "X-USER-ID") Long userId,
@@ -25,6 +27,7 @@ public class AirDropController {
         return airDropService.send(userId, roomId, payload);
     }
 
+    @ApiOperation(value = "Receive the money", notes = "뿌린 금액 수령")
     @PostMapping("/airdrops/{token}")
     public int receive(
         @RequestHeader(name = "X-USER-ID") Long userId,
@@ -33,6 +36,7 @@ public class AirDropController {
         return airDropService.receive(userId, roomId, token);
     }
 
+    @ApiOperation(value = "Query the event status", notes = "뿌리기 이벤트 조회")
     @GetMapping("/airdrops/{token}")
     public AirDropResponse status(
         @RequestHeader(name = "X-USER-ID") Long userId,
